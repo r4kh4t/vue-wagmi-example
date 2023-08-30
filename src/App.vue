2a408f037476c77f5e328774cef5bf6ce60dbe28
Rakhat Shakimbekov <rahateamfor@gmail.com>
2023-08-30 18:10:39 +0800

    test: test repo

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { mainnet, arbitrum } from '@wagmi/core/chains'
+import { createWeb3Modal, useWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/vue'
+
+// 1. Get projectId
+const projectId = import.meta.env.VITE_PROJECT_ID
+if (!projectId) {
+  throw new Error('VITE_PROJECT_ID is not set')
+}
+
+// 2. Create wagmiConfig
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: 'Web3Modal' })
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig,
+  projectId,
+  chains,
+  themeMode: 'light',
+  themeVariables: {
+    '--w3m-color-mix': '#00BB7F',
+    '--w3m-color-mix-strength': 40
+  }
+})
+
+// 4. Use modal composable
+const modal = useWeb3Modal()
+</script>
+
+<template>
+  <button @click="modal.open()">Open Connect Modal</button>
+  <button @click="modal.open({ view: 'Networks' })">Open Network Modal</button>
+</template>
